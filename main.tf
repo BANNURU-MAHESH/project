6f0454b955f6ca3353bc41419e9eb616aa0fa463
root <root@ip-172-31-13-224.ec2.internal>
2022-12-05 04:06:37 +0000

    hdjhuefh

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
 provider "aws" {
   region = "us-east-1"
-  aws_access_key_id = "AKIATCKLJBDOB4QUDAUQ"
-  aws_secret_access_key = "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"
+  access_key = "AKIATCKLJBDOB4QUDAUQ"
+  secret_key = "79IWvpgbqqlAk3g0aUeSCgHvHekHE2Fgnbew8zHJ"
 }
 
 resource "aws_instance" "example" {
  for_each = {
     host-1= "ANSIBLE"
-    host-2= "DOCKER"
+    host-2= "Docker"
 }
   ami           = "ami-02b972fec07f1e659"
   instance_type = "t2.medium"
   tags= {
-    Name = "${each.value}"
+    Name = "${each.value}" 
   }
 }
 
